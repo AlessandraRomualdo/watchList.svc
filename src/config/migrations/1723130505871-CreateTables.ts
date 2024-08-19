93d87c465c9ae52ad4d3945b89a26225a5bb4dc4
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2024-08-19 09:25:53 -0300

    fix - migrations

--- src/config/migrations/1723130505871-CreateTables.ts
+++ src/config/migrations/1723130505871-CreateTables.ts
@@ -4,8 +4,8 @@ export class CreateTables1723130505871 implements MigrationInterface {
   name = 'CreateTables1723130505871';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE DATABASE IF NOT EXISTS "db_watchList"`);
-    await queryRunner.query(`USE "db_watchList"`);
+    // await queryRunner.query(`CREATE DATABASE IF NOT EXISTS "db_watchList"`);
+    // await queryRunner.query(`USE "db_watchList"`);
 
     await queryRunner.query(
       `CREATE TABLE IF NOT EXISTS "list_serie" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "listId" uuid, "serieId" uuid, CONSTRAINT "PK_7e8d0d120d62fecb4cc2a89c3b3" PRIMARY KEY ("id"))`,
@@ -40,9 +40,15 @@ export class CreateTables1723130505871 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "serie" ADD CONSTRAINT "FK_bc8ec299721c8ff23d333145ed9" FOREIGN KEY ("genderId") REFERENCES "gender"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
+    // await queryRunner.query(
+    //   `ALTER TABLE "serie" ADD CONSTRAINT "FK_f2bedb0597d26501c3a80384594" FOREIGN KEY ("listSeriesId") REFERENCES "list_serie"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    // );
     await queryRunner.query(
       `ALTER TABLE "movie" ADD CONSTRAINT "FK_902bb01f6e71899b094c808efb4" FOREIGN KEY ("genderId") REFERENCES "gender"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
+    // await queryRunner.query(
+    //   `ALTER TABLE "movie" ADD CONSTRAINT "FK_9486e35306c6472f80c4714c05f" FOREIGN KEY ("listMoviesId") REFERENCES "list_movie"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    // );
     await queryRunner.query(
       `ALTER TABLE "list_movie" ADD CONSTRAINT "FK_28c07fc26680987da9d4aff1f1b" FOREIGN KEY ("listId") REFERENCES "list"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
@@ -58,45 +64,47 @@ export class CreateTables1723130505871 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "user" DROP CONSTRAINT "FK_c28e52f758e7bbc53828db92194"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "list" DROP CONSTRAINT "FK_46ded14b26382088c9f032f8953"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "list_movie" DROP CONSTRAINT "FK_d3f94afb2d0e54ef7377ebcbeac"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "list_movie" DROP CONSTRAINT "FK_28c07fc26680987da9d4aff1f1b"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "movie" DROP CONSTRAINT "FK_9486e35306c6472f80c4714c05f"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "movie" DROP CONSTRAINT "FK_902bb01f6e71899b094c808efb4"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "serie" DROP CONSTRAINT "FK_f2bedb0597d26501c3a80384594"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "serie" DROP CONSTRAINT "FK_bc8ec299721c8ff23d333145ed9"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "list_serie" DROP CONSTRAINT "FK_c61742ddd81e39d116fa7c1f828"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "list_serie" DROP CONSTRAINT "FK_dda0b1dc9a92ee0a0a76c4b3f4b"`,
-    );
-    await queryRunner.query(`DROP TABLE "user"`);
-    await queryRunner.query(`DROP TABLE "role"`);
-    await queryRunner.query(`DROP TABLE "list"`);
-    await queryRunner.query(`DROP TABLE "list_movie"`);
-    await queryRunner.query(`DROP TABLE "movie"`);
-    await queryRunner.query(`DROP TABLE "gender"`);
-    await queryRunner.query(`DROP TABLE "serie"`);
-    await queryRunner.query(`DROP TABLE "list_serie"`);
-    await queryRunner.query(`DROP TABLE IF EXISTS "list_serie"`);
-    await queryRunner.query(`DROP DATABASE IF EXISTS "db_watchList"`);
+    await queryRunner.startTransaction();
+    try {
+      await queryRunner.query(
+        `ALTER TABLE "user" DROP CONSTRAINT "FK_c28e52f758e7bbc53828db92194"`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "list" DROP CONSTRAINT "FK_46ded14b26382088c9f032f8953"`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "list_movie" DROP CONSTRAINT "FK_d3f94afb2d0e54ef7377ebcbeac"`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "list_movie" DROP CONSTRAINT "FK_28c07fc26680987da9d4aff1f1b"`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "movie" DROP CONSTRAINT "FK_902bb01f6e71899b094c808efb4"`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "serie" DROP CONSTRAINT "FK_bc8ec299721c8ff23d333145ed9"`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "list_serie" DROP CONSTRAINT "FK_c61742ddd81e39d116fa7c1f828"`,
+      );
+      await queryRunner.query(
+        `ALTER TABLE "list_serie" DROP CONSTRAINT "FK_dda0b1dc9a92ee0a0a76c4b3f4b"`,
+      );
+      await queryRunner.query(`DROP TABLE "user"`);
+      await queryRunner.query(`DROP TABLE "role"`);
+      await queryRunner.query(`DROP TABLE "list"`);
+      await queryRunner.query(`DROP TABLE "list_movie"`);
+      await queryRunner.query(`DROP TABLE "movie"`);
+      await queryRunner.query(`DROP TABLE "serie"`);
+      await queryRunner.query(`DROP TABLE "gender"`);
+      await queryRunner.query(`DROP TABLE "list_serie"`);
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
+
+    // Executar DROP DATABASE fora da transação
+    // await queryRunner.query(`DROP DATABASE IF EXISTS "db_watchList"`);
   }
 }
